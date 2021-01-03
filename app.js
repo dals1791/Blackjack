af7f7ea7ab5104499b750a9a675a4eb29302d58a
Sean Daly <daly.se91@gmail.com>
2021-01-02 19:48:52 -0600

    Added scoring functions

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ let cardDeck = []
     }
 
 createCardDeck()
+
 // ------------------------------------------------------------
 // Pulls a random card from the deck and removes it from the deck.
 const createCard = function (){
@@ -54,17 +55,55 @@ const createCard = function (){
     const $div = $("<div>")
     $div.addClass("card")
     $div.html(card.description)
+    $div.attr("value", card.value)
     
     cardDeck.splice(randCard, 1)
     return $div
+    // console.log(cardValue)
 }
 const playerScore = function (){
-    const playerScore = null
-    const $cardValues = $("section .playerCards").children()
-    // add foreach function to loop through and add value of each child
+    
+    const cardValues = $("section .playerCards").children()
+    
+    // console.log("*******cardValues******")
+    const cardValuesArray = $.makeArray(cardValues).map((card, idx) => {
+        // console.log(card.attributes.value.value)
+        return parseInt(card.attributes.value.value)
+    })
+    // console.log("*******cardValuesArray******")
+    // console.log(cardValuesArray)
+    const adder = function (accumulator, value){
+        return accumulator+value
+    }
+    const playerScore = "Player has " + cardValuesArray.reduce(adder)
 
+    console.log(playerScore)
+    return playerScore
+    
 
 }
+const dealerScore = function (){
+    
+    const cardValues = $("section .dealerCards").children()
+    
+    // console.log("*******cardValues******")
+    const cardValuesArray = $.makeArray(cardValues).map((card, idx) => {
+        // console.log(card.attributes.value.value)
+        return parseInt(card.attributes.value.value)
+    })
+    // console.log("*******cardValuesArray******")
+    // console.log(cardValuesArray)
+    const adder = function (accumulator, value){
+        return accumulator+value
+    }
+    const dealerScore = "Dealer has " + cardValuesArray.reduce(adder)
+
+    console.log(dealerScore)
+    return dealerScore
+    
+
+}
+
 
 // ------------------------------------------------------------
 // Deals two starting cards to the player and dealer
@@ -83,6 +122,9 @@ const dealCards = function (){
         $playerSection.append(playerCard2)
         $dealerSection.append(dealerCard1)
         $dealerSection.append(dealerCard2)
+        
+        playerScore()
+        dealerScore()
     })
 }
 dealCards()
@@ -90,14 +132,56 @@ dealCards()
 
 // ------------------------------------------------------------
 // Adds card to player hands
-const hitCard = function (){
+const playerHitCard = function (){
     const $hitCard = $("button.hitButton")
     $hitCard.on("click", (event)=>{
     let newCard = createCard()
     const $playerCards = $(".playerCards")
     $playerCards.append(newCard)
+    playerScore()
 })
 }
-hitCard()
+playerHitCard()
 
+const dealerHit = function (){
+   if(dealerScore<17){
+    let newCard = createCard()
+    const $dealerCards = $(".dealerCards")
+    $dealerCards.append(newCard)
+    dealerScore()
+}
+    
+
+
+}
+
+const gameRules = function (){
+    const score = $("<div>")
+    const score2 = $("<div>")
+    const victorySpot = $(".player")
+    const victorySpot2 = $(".dealer")
+    if (dealerScore===21){
+        
+        score2.html("Dealer Wins")
+        score.html("Player Wins")
+        victorySpot.append(score)
+        victorySpot2.append(score2)
+
+    }
+    else if (playerScore ===21){
+        score.html("BlackJack")
+        
+        victorySpot.append(score)
+    }
+    else if (playerScore===21 && dealerScore===21){
+        
+        score.html("Player Pushes")
+        const victorySpot = $(".player")
+        victorySpot.append(score)
+    }
+    else{
+        score.html("Player Busts, Dealer Wins")
+    }
+
+}
 
